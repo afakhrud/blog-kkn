7428317664593521b5b4e25bd1f9919fac32cec5
Ahmad Fakhrudin A <ahmad_fakhrudin@outlook.com>
2021-08-08 12:35:27 +0700

    first commit

---
+++ components/EditorTiny.jsx
@@ -0,0 +1,85 @@
+import React, { useRef } from 'react'
+import { Editor } from '@tinymce/tinymce-react'
+
+const apiKey = '2g71q5ct7arfflwsnlm94sh4y4xj5kq6uja48zejzulxh5sn'; 
+
+function EditorTiny(props) {
+    const editorRef = useRef(null);
+    const log = () => {
+        if (editorRef.current) {
+            console.log(editorRef.current.getContent());
+        }
+    };
+
+    return (
+        <div>
+             <Editor
+                apiKey={apiKey}
+                onInit={(evt, editor) => editorRef.current = editor}
+                initialValue="<p>This is the initial content of the editor.</p>"
+                init={{
+                height: 500,
+                menubar: 'file edit view insert format tools table tc help',
+                plugins: [
+                    'print preview powerpaste casechange importcss tinydrive searchreplace autolink autosave save directionality advcode visualblocks visualchars fullscreen image link media mediaembed template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists checklist wordcount tinymcespellchecker a11ychecker imagetools textpattern noneditable help formatpainter permanentpen pageembed charmap tinycomments mentions quickbars linkchecker emoticons advtable export'
+                ],
+                toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | a11ycheck ltr rtl | showcomments addcomment',
+                content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+                autosave_ask_before_unload: true,
+                autosave_interval: '30s',
+                autosave_prefix: '{path}{query}-{id}-',
+                autosave_restore_when_empty: false,
+                autosave_retention: '2m',
+                image_advtab: true,
+                link_list: [
+                    { title: 'My page 1', value: 'https://www.tiny.cloud' },
+                    { title: 'My page 2', value: 'http://www.moxiecode.com' }
+                ],
+                image_list: [
+                    { title: 'My page 1', value: 'https://www.tiny.cloud' },
+                    { title: 'My page 2', value: 'http://www.moxiecode.com' }
+                ],
+                image_class_list: [
+                    { title: 'None', value: '' },
+                    { title: 'Some class', value: 'class-name' }
+                ],
+                importcss_append: true,
+                templates: [
+                        { title: 'New Table', description: 'creates a new table', content: '<div class="mceTmpl"><table width="98%%"  border="0" cellspacing="0" cellpadding="0"><tr><th scope="col"> </th><th scope="col"> </th></tr><tr><td> </td><td> </td></tr></table></div>' },
+                    { title: 'Starting my story', description: 'A cure for writers block', content: 'Once upon a time...' },
+                    { title: 'New list with dates', description: 'New List with dates', content: '<div class="mceTmpl"><span class="cdate">cdate</span><br /><span class="mdate">mdate</span><h2>My List</h2><ul><li></li><li></li></ul></div>' }
+                ],
+                template_cdate_format: '[Date Created (CDATE): %m/%d/%Y : %H:%M:%S]',
+                template_mdate_format: '[Date Modified (MDATE): %m/%d/%Y : %H:%M:%S]',
+                height: 600,
+                image_caption: true,
+                quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
+                noneditable_noneditable_class: 'mceNonEditable',
+                toolbar_mode: 'sliding',
+                spellchecker_ignore_list: ['Ephox', 'Moxiecode'],
+                tinycomments_mode: 'embedded',
+                content_style: '.mymention{ color: gray; }',
+                contextmenu: 'link image imagetools table configurepermanentpen',
+                a11y_advanced_options: true,
+                // skin: useDarkMode ? 'oxide-dark' : 'oxide',
+                // content_css: useDarkMode ? 'dark' : 'default',
+                /*
+                The following settings require more configuration than shown here.
+                For information on configuring the mentions plugin, see:
+                https://www.tiny.cloud/docs/plugins/premium/mentions/.
+                */
+                mentions_selector: '.mymention',
+                // mentions_fetch: mentions_fetch,
+                // mentions_menu_hover: mentions_menu_hover,
+                // mentions_menu_complete: mentions_menu_complete,
+                // mentions_select: mentions_select,
+                mentions_item_type: 'profile'
+                }}
+            />
+            <button onClick={log}>Generate HTML</button>
+        </div>
+    )
+}
+
+export default EditorTiny
+// plugins: 'print preview powerpaste casechange importcss tinydrive searchreplace autolink autosave save directionality advcode visualblocks visualchars fullscreen image link media mediaembed template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists checklist wordcount tinymcespellchecker a11ychecker imagetools textpattern noneditable help formatpainter permanentpen pageembed charmap tinycomments mentions quickbars linkchecker emoticons advtable export',
